2ad29b1d29224a80ea46f2c6cc0362ac180cecbf
okoye-collins <collins.kcj@gmail.com>
2022-08-05 08:05:39 +0100

    done with navbar(responsiveness)

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import './App.css'
+import Navbar from './components/navbar-section/Navbar'
 
 function App() {
   return (
     <div className="App">
-
+      <Navbar />
     </div>
   )
 }
--- src/components/navbar-section/Navbar.jsx
+++ src/components/navbar-section/Navbar.jsx
@@ -1,7 +1,47 @@
+import './Navbar.css'
+import logo from '../../assets/img/logo.png'
+import menu from '../../assets/img/menu.png'
+import close from '../../assets/img/close.png'
+
+import { useState } from 'react'
+
+
 const Navbar = () => {
-    return ( 
-        <nav>
+
+    const [menuIcon, setmenuIcon] = useState(menu)
+    const [displaymenu, setdisplayMenu] = useState(false)
+
+    const handlemenu = (close, e) =>{
+        if(menuIcon == menu){
+            e.target.classList.add('close-bar')
+            setmenuIcon(close)
+            setdisplayMenu(true)
+        }
+        else{
+            setmenuIcon(menu)
+            setdisplayMenu(false)
             
+        }
+        
+    }
+
+    return ( 
+
+        <nav>
+            <div className="container">
+                <div className="container-content">
+                    <img src={logo} alt="site-logo" className='logo'/>
+
+                    <img onClick={(e) => handlemenu(close, e)} src={menuIcon} alt="hanbuger menu" className='hanbuger-menu' />
+                </div>
+
+                <ul className={displaymenu? 'menu-item display-list': 'menu-item'}>
+                    <li><a href="#" className='active'>Home</a></li>
+                    <li><a href="#">Product</a></li>
+                    <li><a href="#">Faq</a></li>
+                    <li><a href="#">Contact</a></li>
+                </ul>
+            </div>
         </nav>
     );
 }
